cfb135b4905a0130c3adacb5ac157aa9f7d48faf
suxiao <suxiao@mixiaojin.com>
2022-07-13 10:34:32 +0800

    feat: modal入口文件index

---
+++ src/modal/index.tsx
@@ -0,0 +1,6 @@
+import OfficialModal from "./OfficialModal";
+import ModalProps from "./ModalProps";
+import RefModal from "./RefModal";
+import EmitModal from "./EmitModal";
+
+export { OfficialModal, ModalProps, RefModal, EmitModal };
