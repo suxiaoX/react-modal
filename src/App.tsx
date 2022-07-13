767da75136568d83b9af6dc220c1d728669b0350
suxiao <suxiao@mixiaojin.com>
2022-07-13 10:33:08 +0800

    feat: 入口文件修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import "antd/dist/antd.min.css";
+import { OfficialModal, ModalProps, RefModal, EmitModal } from "./modal";
+import { ModalRefType } from "./modal/RefModal";
+import "./App.css";
 
 function App() {
+  const refModal = React.useRef<ModalRefType>(null);
+  const showRefModal = () => {
+    refModal.current?.show();
+  };
+  const showEmitModal = () => {
+    EmitModal.show({
+      onOk: () => {
+        console.log("onOk");
+      },
+      onCancel: () => {
+        console.log("onCancel");
+      },
+    });
+  };
+  const showEmitModal2 = () => {
+    EmitModal.show({
+      onOk: () => {
+        console.log("onOk2");
+      },
+      onCancel: () => {
+        console.log("onCancel2");
+      },
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OfficialModal />
+      <ModalProps>
+        <Button type="primary">Props Modal</Button>
+      </ModalProps>
+      <RefModal ref={refModal}></RefModal>
+      <Button onClick={showRefModal}> 打开refModal </Button>
+      <Button type="primary" onClick={showEmitModal}>
+        打开EmitModal
+      </Button>
+      <Button type="primary" onClick={showEmitModal2}>
+        打开EmitModal2
+      </Button>
+      <EmitModal />
     </div>
   );
 }
